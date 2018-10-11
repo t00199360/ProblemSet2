c4dac20c9a52267ecc2b18512faf7cf6e2c4ffe5
t00199360 <t00199360@students.ittralee.ie>
2018-10-11 10:32:23 +0100

    worked on bicycle.java, have a databases CA next though so not much work done today

--- src/Bicycle.java
+++ src/Bicycle.java
@@ -4,9 +4,9 @@ public class Bicycle
     private double value;
     private String make;
 
-    //mutator methods
+    //accessor methods
 
-    public String setOwnersName()
+    public String getOwnersName()
     {
         return ownersName;
     }
@@ -19,5 +19,48 @@ public class Bicycle
     public String getMake()
     {
         return make;
+    }
+
+    //mutator methods
+
+
+    public void setMake(String make)
+    {
+        this.make = make;
+    }
+
+    public void setOwnersName(String ownersName)
+    {
+        this.ownersName = ownersName;
+    }
+
+    public void setValue(double value)
+    {
+        this.value = value;
+    }
+
+    //no argument constructor
+    public Bicycle()
+    {
+        this.ownersName="unknown owner";
+        this.make="unknown make";
+        this.value=0.00;
+    }
+
+    //3 argument constructor
+    public Bicycle(String ownersName, String make, double value)
+    {
+        this.ownersName=ownersName;
+        this.make=make;
+        this.value=value;
+    }
+
+
+
+    //toString() method
+    @Override
+    public String toString()
+    {
+        return "afbsdfgs";
     }
 }
